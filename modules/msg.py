6474aa9b9f9f590e4851bf769aec00f88a31aeb8
jmcc-137 <jmcrispinc20@gmail.com>
2025-03-06 15:56:32 -0500

    docs: :memo: Cambio del menu a msg

---
+++ modules/msg.py
@@ -0,0 +1,65 @@
+MAIN_MENU="""
+⸻⸻⸻⸻⸻⸻⸻
+       🌔BIENVENIDO🌔
+⸻⸻⸻⸻⸻⸻⸻
+1.AGREGAR PRODUCTO
+2.ELIMINAR PRODUCTO
+3.EDITAR PRODUCTO
+4.BUSCAR PRODUCTO
+5.MOSTRAR PRODUCTOS
+6.SALIR
+⸻⸻⸻⸻⸻⸻⸻
+"""
+
+ADD_MENU="""
+⸻⸻⸻⸻⸻⸻⸻
+        🍷AGREGAR🍷
+⸻⸻⸻⸻⸻⸻⸻
+1.AGREGAR CERVEZA
+2.AGREGAR VINO
+3.AGREGAR LICORES
+4.VOLVER 
+⸻⸻⸻⸻⸻⸻⸻     
+"""
+
+DELETE_MENU="""
+⸻⸻⸻⸻⸻⸻⸻
+        🍷ELIMINAR🍷
+⸻⸻⸻⸻⸻⸻⸻
+1.ELIMINAR CERVEZA
+2.ELIMINAR VINO
+3.ELIMINAR LICORES
+4.VOLVER
+"""
+
+
+SEARCH_MENU = """
+⸻⸻⸻⸻⸻⸻⸻
+        🍷BUSCAR🍷
+⸻⸻⸻⸻⸻⸻⸻
+1.BUSCAR CERVEZA
+2.BUSCAR VINO
+3.BUSCAR LICORES
+4.VOLVER
+"""
+EDIT_MENU = """
+⸻⸻⸻⸻⸻⸻⸻
+        🍷BUSCAR🍷
+⸻⸻⸻⸻⸻⸻⸻
+1.EDITAR NOMBRE
+2.EDITAR VINO
+3.EDITAR LICORES
+4.VOLVER
+"""
+SEE_MENU="""
+⸻⸻⸻⸻⸻⸻⸻
+        🍷MOSTRAR🍷
+⸻⸻⸻⸻⸻⸻⸻
+1.MOSTRAR TODOS LOS PRODUCTOS 
+2.MOSTRAR EN ORDEN ALFABETICO
+3.MOSTRAR CERVEZAS
+4.MOSTRAR VINOS
+5.MOSTRAR LICORES
+6.VOLVER
+"""
+
