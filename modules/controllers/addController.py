f32936f9d44d047f14b41c8c4408e653d2f90db8
LFDIAZDEV2209 <201288669+LFDIAZDEV2209@users.noreply.github.com>
2025-03-09 02:43:47 -0500

    feat: :sparkles: Funcion pregunta al usuario si desea agregar otro producto

--- modules/controllers/addController.py
+++ modules/controllers/addController.py
@@ -11,78 +11,105 @@ def addMenu():
     option = input("= ")
     match option:
         case "1":
-            sc.limpiar_pantalla()
-            idBeer = random.randint(1023, 9876)
-            nomBeer = vd.validatetext("Ingrese el nombre de la cerveza: ")
-            ml= vd.validateInt("Ingrese los ml: ")
-            costBeer = vd.validateInt("Costo: ")
-            priceBeer = vd.validateInt("Precio de venta: ")
+            while True:
+                sc.limpiar_pantalla()
+                idBeer = random.randint(1023, 9876)
+                nomBeer = vd.validatetext("Ingrese el nombre de la cerveza: ")
+                ml= vd.validateInt("Ingrese los ml: ")
+                costBeer = vd.validateInt("Costo: ")
+                priceBeer = vd.validateInt("Precio de venta: ")
 
-            CERVEZA = {
-                idBeer:{
-                "Name":nomBeer,
-                "ml":ml,
-                "Cost":costBeer,
-                "Price":priceBeer
+                CERVEZA = {
+                    idBeer:{
+                    "Name":nomBeer,
+                    "ml":ml,
+                    "Cost":costBeer,
+                    "Price":priceBeer
 
+                    }
                 }
-            }
-            if not cf.updateJson(CERVEZA,["Beer"]):
-                print("Cerveza agregada exitosamente ✅")
-                cf.printTable("Beer", CERVEZA, idBeer)
-            else:
-                print("No se pudo agregar la cerveza ❌ ")
+                if not cf.updateJson(CERVEZA,["Beer"]):
+                    print("Cerveza agregada exitosamente ✅")
+                    cf.printTable("Beer", CERVEZA, idBeer)
+                else:
+                    print("No se pudo agregar la cerveza ❌ ")
+                
+                while True:
+                    opcion = input("\n¿Desea agregar otra cerveza? (si/no): ").strip().lower()
+                    if opcion == "si":
+                        break  
+                    elif opcion == "no":
+                        sc.pausar_pantalla()
+                        return addMenu()  
+                    else:
+                        print("⚠️ Respuesta inválida. Por favor, ingrese 'si' o 'no'.")
 
-            sc.pausar_pantalla()
-            return addMenu()
         case "2":
-            sc.limpiar_pantalla()
-            idVino = random.randint(1023, 9876)
-            nomVino = vd.validatetext("Ingrese el nombre del vino: ")
-            mlVino= vd.validateInt("Ingrese los ml: ")
-            costVino = vd.validateInt("Costo: ")
-            priceVino = vd.validateInt("Precio de venta: ")
+            while True:
+                sc.limpiar_pantalla()
+                idVino = random.randint(1023, 9876)
+                nomVino = vd.validatetext("Ingrese el nombre del vino: ")
+                mlVino= vd.validateInt("Ingrese los ml: ")
+                costVino = vd.validateInt("Costo: ")
+                priceVino = vd.validateInt("Precio de venta: ")
 
-            VINO= {
-                idVino:{
-                "Name":nomVino,
-                "ml":mlVino,
-                "Cost":costVino,
-                "Price":priceVino
+                VINO= {
+                    idVino:{
+                    "Name":nomVino,
+                    "ml":mlVino,
+                    "Cost":costVino,
+                    "Price":priceVino
 
+                    }
                 }
-            }
-            if not cf.updateJson(VINO,["Wine"]):
-                print("Vino agregado exitosamente ✅")
-                cf.printTable("Wine", VINO, idVino)
-            else:
-                print("No se pudo agregar el vino ❌")
-            sc.pausar_pantalla()
-            return addMenu()
+                if not cf.updateJson(VINO,["Wine"]):
+                    print("Vino agregado exitosamente ✅")
+                    cf.printTable("Wine", VINO, idVino)
+                else:
+                    print("No se pudo agregar el vino ❌")
+                while True:
+                    opcion = input("\n¿Desea agregar otro vino? (si/no): ").strip().lower()
+                    if opcion == "si":
+                        break  
+                    elif opcion == "no":
+                        sc.pausar_pantalla()
+                        return addMenu()  
+                    else:
+                        print("⚠️ Respuesta inválida. Por favor, ingrese 'si' o 'no'.")
+
         case "3":
-            sc.limpiar_pantalla()
-            idLiquors = random.randint(1023, 9876)
-            nomLiquors = vd.validatetext("Ingrese el nombre del licor: ")
-            mlLiquors= vd.validateInt("Ingrese los ml: ")
-            costLiquors = vd.validateInt("Costo: ")
-            priceLiquors = vd.validateInt("Precio de venta: ")
+            while True:
+                sc.limpiar_pantalla()
+                idLiquors = random.randint(1023, 9876)
+                nomLiquors = vd.validatetext("Ingrese el nombre del licor: ")
+                mlLiquors= vd.validateInt("Ingrese los ml: ")
+                costLiquors = vd.validateInt("Costo: ")
+                priceLiquors = vd.validateInt("Precio de venta: ")
 
-            LICORES = {
-                idLiquors:{
-                "Name":nomLiquors,
-                "ml":mlLiquors,
-                "Cost":costLiquors,
-                "Price":priceLiquors
+                LICORES = {
+                    idLiquors:{
+                    "Name":nomLiquors,
+                    "ml":mlLiquors,
+                    "Cost":costLiquors,
+                    "Price":priceLiquors
 
+                    }
                 }
-            }
-            if not cf.updateJson(LICORES,["Liquors"]):
-                print("Licor agregado exitosamente ✅")
-                cf.printTable("Liquors", LICORES, idLiquors)
-            else:
-                print("No se pudo agregar el licor ❌")
-            sc.pausar_pantalla()
-            return addMenu()
+                if not cf.updateJson(LICORES,["Liquors"]):
+                    print("Licor agregado exitosamente ✅")
+                    cf.printTable("Liquors", LICORES, idLiquors)
+                else:
+                    print("No se pudo agregar el licor ❌")
+                while True:
+                    opcion = input("\n¿Desea agregar otro licor? (si/no): ").strip().lower()
+                    if opcion == "si":
+                        break  
+                    elif opcion == "no":
+                        sc.pausar_pantalla()
+                        return addMenu()  
+                    else:
+                        print("⚠️ Respuesta inválida. Por favor, ingrese 'si' o 'no'.")
+
         case "4":
             pass
         case _:
